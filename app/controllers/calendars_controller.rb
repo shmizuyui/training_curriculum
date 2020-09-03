4b7e22ad826e077b7b055eedaa89490ef5acb4a0
shmizuyui <yuihana760@gmail.com>
2020-09-03 18:02:12 +0900

    Revert "lssue6"
    This reverts commit a6ae1ad7554dcbfc237097742ca2b82f8dac32a6.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,11 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num =0
-      if wday_num <= 7
-        wday_num = wday_num - 7
-      end
-      days = { :month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: today_wdays}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
 
